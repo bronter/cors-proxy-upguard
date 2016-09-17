61436fc65ebeed239a727c747d0ae2a620417828
Gabriel Charleboix <gcharleboix@gmail.com>
2016-09-16 23:04:48 -0700

    standard engine => fasthttp engine

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/bronter/cors-proxy-upguard/resources"
-	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/engine/fasthttp"
 )
 
 func main() {
 	// Start the server
 	println("Listening at :9000")
-	resources.NewRouter().Run(standard.New(":9000"))
+	resources.NewRouter().Run(fasthttp.New(":9000"))
 }
